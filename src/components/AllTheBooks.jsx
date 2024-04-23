2ec76271ed2699fd3b71caf28ebebb43faf355ed
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-04-23 16:58:05 +0200

    v0.5

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -2,20 +2,20 @@
 import Card from 'react-bootstrap/Card';
 // import exampleImg from '../assets/images/the-deep-sky.jpg'
 import { Col, Container, Row } from 'react-bootstrap';
-import books from '../data/books/fantasy.json'
+import fantasyBooks from '../data/books/fantasy.json'
 import star from '../assets/images/star.svg'
 
 function AllTheBooks() {
     return (
       <Container>
-        <Row className='align-items-center my-5 justify-content-center'>
-            <Col xs={12}>
+        <h5 className='mt-4 mb-3 fw-bold fs-3'>Fantasy Books</h5>
 
-                <h5 className='mb-5 fw-bold fs-3'>Fantasy Books</h5>
+        <Row className='align-items-center justify-content-center row-cols-xs-1 row-cols-md-3 row-cols-lg-6 gap-3 mb-4'>
 
-                {books.map((book, i) => {
+                {fantasyBooks.map((book, i) => {
                     return (
-                <Card className='book-card' key={`book-${i}`}>
+                <Col key={`book-${i}`}>
+                <Card className='book-card'>
                 <Card.Img className='card-img-top' variant="top" src={book.img}/>
                 <Card.Body className='card-body'>
                     <Card.Title className='card-title'>{book.title}</Card.Title>
@@ -33,10 +33,10 @@ function AllTheBooks() {
                     </div>
                 </Card.Body>
                 </Card>
+                </Col>
                     )
                 })}
 
-            </Col>
         </Row>
     </Container>
   );
